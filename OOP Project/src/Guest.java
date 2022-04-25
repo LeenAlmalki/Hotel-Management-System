8d171f5af529785e821a1555cc3c128337c6c821
RayanahAlsubaie9 <102022003+RayanahAlsubaie9@users.noreply.github.com>
2022-04-26 01:44:05 +0300

    Merge branch 'main' of https://github.com/LeenAlmalki/Hotel-Management-System.git into main

--- OOP Project/src/Guest.java
+++ OOP Project/src/Guest.java
@@ -1,5 +1,9 @@
 
 public class Guest {
+	private int guest_id;
+	private String guest_name;
+	private int phone_number;
+	private String email;
 
 	public Guest() {
 		// TODO Auto-generated constructor stub
