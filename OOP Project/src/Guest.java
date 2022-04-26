3d0bf1fb56cf09ad3a7bac23b2ff614c914fc9a2
RayanahAlsubaie9 <102022003+RayanahAlsubaie9@users.noreply.github.com>
2022-04-26 04:00:45 +0300

    update

--- OOP Project/src/Guest.java
+++ OOP Project/src/Guest.java
@@ -9,4 +9,36 @@ public class Guest {
 		// TODO Auto-generated constructor stub
 	}
 
+	public int getGuestID() {
+		return guestID;
+	}
+
+	public void setGuestID(int guestID) {
+		this.guestID = guestID;
+	}
+
+	public String getGuestName() {
+		return guestName;
+	}
+
+	public void setGuestName(String guestName) {
+		this.guestName = guestName;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
