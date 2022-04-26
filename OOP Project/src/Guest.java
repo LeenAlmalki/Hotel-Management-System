ab59fbee5e21aeb02146eca7205eaffb4e8ff023
RayanahAlsubaie9 <102022003+RayanahAlsubaie9@users.noreply.github.com>
2022-04-26 22:26:19 +0300

    Merge branch 'main' of https://github.com/LeenAlmalki/Hotel-Management-System.git into main

--- OOP Project/src/Guest.java
+++ OOP Project/src/Guest.java
@@ -1,18 +1,46 @@
 
-public class Guest extends Person{
+public class Guest{
 	private int guestID;
+	private String name;
+	private String phoneNumber;
+	private String email;
 	//here
 	private boolean checkedIn;
 	private Room room;
 
 	public Guest() {
-		super();
+		name = null;
+		phoneNumber = null;
+		email = null;
 		guestID = 0;
 	}
 	
 	public Guest(String name, String phoneNumber, String email, int guestID) {
-		super(name, phoneNumber, email);
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
 		this.guestID = guestID;
+	}
+	
+	public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+	public String getphoneNumber() {
+		return phoneNumber;
+	}
+	public void setphoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public int getGuestID() {
