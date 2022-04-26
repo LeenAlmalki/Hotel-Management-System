100b2b92d7719dec419f7bb83aa725fb0c2d8656
RayanahAlsubaie9 <102022003+RayanahAlsubaie9@users.noreply.github.com>
2022-04-26 22:32:54 +0300

    update

--- OOP Project/src/Guest.java
+++ OOP Project/src/Guest.java
@@ -1,5 +1,28 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-public class Guest{
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.BoxLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import com.jgoodies.forms.layout.RowSpec;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.DropMode;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Guest extends JFrame {
+	private JTextField txtGfdhd;
+
+	//start
 	private int guestID;
 	private String name;
 	private String phoneNumber;
@@ -7,13 +30,53 @@ public class Guest{
 	//here
 	private boolean checkedIn;
 	private Room room;
-
-	public Guest() {
-		name = null;
-		phoneNumber = null;
-		email = null;
-		guestID = 0;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Guest frame = new Guest();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Guest() {
+		setTitle("Guest");
+		getContentPane().setBackground(new Color(238, 238, 238));
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Guest ID ");
+		lblNewLabel.setBounds(14, 92, 66, 33);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		getContentPane().add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Enter");
+		btnNewButton.setBounds(138, 224, 147, 25);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		getContentPane().add(btnNewButton);
+		
+		txtGfdhd = new JTextField();
+		txtGfdhd.setBounds(94, 92, 137, 33);
+		getContentPane().add(txtGfdhd);
+		txtGfdhd.setColumns(4);
+	}
+
+	
+	//start
+	
 	
 	public Guest(String name, String phoneNumber, String email, int guestID) {
 		this.name = name;
@@ -86,4 +149,8 @@ public class Guest{
 	
 	//payBill
 	//the total price is called from the receptionist class
+
+	
+	
+	
 }
