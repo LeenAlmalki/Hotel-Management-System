8a1dbc1951523ea300163fe2198a2c51680594ae
RayanahAlsubaie9 <102022003+RayanahAlsubaie9@users.noreply.github.com>
2022-04-30 16:54:01 +0300

    holder

--- OOP Project/src/holder.java
+++ OOP Project/src/holder.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 class holder implements Serializable
 {
     Doubleroom luxury_doublerrom[]=new Doubleroom[10]; //Luxury
-    Doubleroom deluxe_doublerrom[]=new Doubleroom[20]; //Deluxe
+    Doubleroom standard_doublerrom[]=new Doubleroom[20]; //Standard
     Singleroom luxury_singleerrom[]=new Singleroom[10]; //Luxury
-    Singleroom deluxe_singleerrom[]=new Singleroom[20]; //Deluxe
+    Singleroom standard_singleerrom[]=new Singleroom[20]; //Standard
 }
